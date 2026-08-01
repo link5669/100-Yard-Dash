cf3dd8fa83ba89eb18e011e26caebc197cbb1521
Miles Acquaviva <milesacq@icloud.com>
2026-08-01 11:41:31 -0400

    fixed camera

--- AnimatedSprite2d.cs
+++ AnimatedSprite2d.cs
@@ -1,17 +1,23 @@
 using Godot;
 using System;
 
-public partial class AnimatedSprite2d : AnimatedSprite2D
-{
+public partial class AnimatedSprite2d : AnimatedSprite2D {
+	double cooldown = 0.0;
 	public override void _Ready() {
 	}
 
 	public override void _Process(double delta) {
+		cooldown -= delta;
+		if (cooldown <= 0.0) {
 		if (Input.IsActionPressed("jump_one")) {
+			this.SpeedScale = 1.0f;
 			this.Play("jump");
+			cooldown = 0.2;
 		}
 		if (Input.IsActionPressed("jump_two")) {
-			
-		}
+			this.SpeedScale = 0.5f;
+			this.Play("jump");
+			cooldown = 0.2;
+		}}
 	}
 }
--- Player.cs
+++ Player.cs
@@ -3,13 +3,24 @@ using System;
 
 public partial class Player : Area2D {
 	public Vector2 ScreenSize; 
-
+	double cooldown = 0.0;
 	public override void _Ready() {
 		ScreenSize = GetViewportRect().Size;
 		this.Position = new Vector2(128,64 + 128);
+		this.Visible = true;
 	}
 
 	public override void _Process(double delta) {
-		
+		cooldown -= delta;
+		if (cooldown <= 0.0) {
+			if (Input.IsActionPressed("jump_one")) {
+				this.Translate(new Vector2(1.1f * (float)delta, 128));
+				cooldown = 0.2;
+			}
+			if (Input.IsActionPressed("jump_two")) {
+				this.Translate(new Vector2(1.1f * (float)delta, 256));
+				cooldown = 0.2;
+			}
+		}
 	}
 }
